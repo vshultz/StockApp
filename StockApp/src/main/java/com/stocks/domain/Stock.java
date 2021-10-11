5f9e8ed2132f00c89e58c9b2dde000c220555f74
Vanessa Shultz <vanessa.shultz12@gmail.com>
2021-10-11 13:58:19 -0500

    Update graph to include year, day, month.

---
+++ StockApp/src/main/java/com/stocks/domain/Stock.java
@@ -0,0 +1,40 @@
+package com.stocks.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity(name = "Stock")
+@Table(name = "stock")
+@Getter
+@Setter
+public class Stock {
+    @EmbeddedId // date, symbol is primary key
+    @GeneratedValue
+    private DateSymbol dateSymbol;
+
+    @Column(name = "year", updatable = false)
+    private int year;
+
+    @Column(name = "month", updatable = false)
+    private int month;
+
+    @Column(name = "day", updatable = false)
+    private int day;
+
+    @Column(name = "open", updatable = false)
+    private float open;
+
+    @Column(name = "high", updatable = false)
+    private float high;
+
+    @Column(name = "low", updatable = false)
+    private float low;
+
+    @Column(name = "close", updatable = false)
+    private float close;
+
+    @Column(name = "volume", updatable = false)
+    private int volume;
+}
