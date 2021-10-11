7fa7cbda65a8342473f12835326ac5135caebf2a
nessa <vanessa.shultz12@gmail.com>
2021-10-10 21:15:03 -0500

    Remove unused imports in controller.

--- StockApp/src/main/java/com/stocks/controllers/SingleGraphController.java
+++ StockApp/src/main/java/com/stocks/controllers/SingleGraphController.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class SingleGraphController {
