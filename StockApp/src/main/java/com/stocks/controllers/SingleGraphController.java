d51de70117ae29419cff14ada2be32fdfcca68d5
Vanessa Shultz <vanessa.shultz12@gmail.com>
2021-10-17 10:07:09 -0500

    Upload working singleGraph chart.

--- StockApp/src/main/java/com/stocks/controllers/SingleGraphController.java
+++ StockApp/src/main/java/com/stocks/controllers/SingleGraphController.java
@@ -1,29 +1,32 @@
 package com.stocks.controllers;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stocks.domain.Stock;
 import com.stocks.services.CompanyStockService;
+import com.stocks.services.JSONCreator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 public class SingleGraphController {
     @Autowired
     CompanyStockService companyStockService;
+    JSONCreator jsonCreator = new JSONCreator();
 
     @PostMapping("/home")
     public String getCompanyInfo(@RequestParam String symbol, Model model) {
         try {
             model.addAttribute("companyName", companyStockService.findName(symbol));
             List<Stock> stockList = companyStockService.findPriceList(symbol);
-            model.addAttribute("stockList", stockList);
-            model.addAttribute("dateList", companyStockService.getBoundaryDates(symbol, stockList));
-            return "responses/index";
-        } catch (IndexOutOfBoundsException i) {
+            String jsonfile = jsonCreator.writeJSONData(stockList, "singleGraph");
+            model.addAttribute("jsonfile", jsonfile);
+            return "candlestockAndVolume";
+        } catch (IndexOutOfBoundsException | FileNotFoundException i) {
             return "error";
         }
     }
@@ -32,5 +35,4 @@ public class SingleGraphController {
     public String index(){
         return "index";
     }
-
 }
--- StockApp/src/main/java/com/stocks/dao/AttributeRepository.java
+++ StockApp/src/main/java/com/stocks/dao/AttributeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
